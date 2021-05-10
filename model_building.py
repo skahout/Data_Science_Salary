901b038beab4dfba06e61117b508d00bec65c2f0
skahout <r.sheryar.k@hotmail.com>
2021-05-09 21:11:40 -0700

    Modified model_building

--- model_building.py
+++ model_building.py
@@ -35,19 +35,29 @@ X_sm = X = sm.add_constant(X)
 model = sm.OLS(y,X_sm)
 model.fit().summary()
 
-from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.model_selection import cross_val_score, GridSearchCV
+
+from sklearn.linear_model import LinearRegression
 
 lm = LinearRegression()
 lm.fit(X_train,y_train)
 
 cross_val_score(lm, X_train, y_train, scoring = 'neg_mean_absolute_error', cv=4) #mean abs error shows how far on avg we are off on our general prediction.
 np.mean(cross_val_score(lm, X_train, y_train, scoring = 'neg_mean_absolute_error', cv=4))
-
 #Around 19.7k Dollars off
 #Matrix is sparse and limited data so for MLR it's difficult to get good values from this 
 
+type(X_train)
+
+# Decision Trees Regression
+from sklearn.tree import DecisionTreeRegressor
+lmt = DecisionTreeRegressor()
+
+np.mean(cross_val_score(lmt,X_train,y_train, scoring = 'neg_mean_absolute_error', cv = 4))
+# $14.7k 
+
+
 # lasso regression
+from sklearn.linear_model import Lasso
 #This would normalize those values so should be better for our models
 lm_l = Lasso(alpha=.03)
 lm_l.fit(X_train,y_train)
@@ -70,6 +80,16 @@ df_err = pd.DataFrame(err, columns = ['aplha','error'])
 df_err[df_err.error == max(df_err.error)]
 # It seems the best error is given when alpha = .03
 
+# Support Vector Regression
+from sklearn.svm import SVR
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+regr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
+regr.fit(X_train,y_train)
+ 
+np.mean(cross_val_score(regr, X_train, y_train, scoring = 'neg_mean_absolute_error', cv=4))
+# Gives 28k Dollars 
 
 # random forest
 from sklearn.ensemble import RandomForestRegressor
@@ -78,43 +98,29 @@ rf = RandomForestRegressor()
 np.mean(cross_val_score(rf,X_train,y_train, scoring = 'neg_mean_absolute_error', cv = 4))
 # error of 14.4k dollars. Much better
 
-
+from sklearn.model_selection import GridSearchCV
 # tune models using GridsearchCV
 parameters = {'n_estimators':range(10,300,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
 gs = GridSearchCV(rf, parameters, scoring = 'neg_mean_absolute_error', cv = 4)
 
 #Takes a lot of time
-#gs.fit(X_train, y_train)
-
+gs.fit(X_train, y_train)
+ 
 gs.best_score_
 gs.best_estimator_
 
 # test ensembles
-tpred_lm = lm.predict(X_test)
-tpred_lml = lm_l.predict(X_test)
-tpred_rf = gs.best_estimator_.predict(X_test)
+tpred_lm = lm.predict(X_test) #linear Regression
+tpred_lmt = lmt.predict(X_test) # Decision Tree
+tpred_lml = lm_l.predict(X_test) # lasso Regression
+tpred_SVR = regr.predict(X_test) # SVR Regression
+tpred_rf = gs.best_estimator_.predict(X_test) # Random Forest
 
 from sklearn.metrics import mean_absolute_error
-mean_absolute_error(y_test,tpred_lm) #18.85310800201176
-mean_absolute_error(y_test,tpred_lml) #19.032844153948414
-mean_absolute_error(y_test,tpred_rf) #12.129961649089166
+mean_absolute_error(y_test,tpred_lm) #18.8k
+mean_absolute_error(y_test,tpred_lmt) #9.9k
+mean_absolute_error(y_test,tpred_lml) #19k
+mean_absolute_error(y_test,tpred_rf) #12k
+mean_absolute_error(y_test,tpred_SVR) #29k
 
 mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2) #14.326232442246416
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
